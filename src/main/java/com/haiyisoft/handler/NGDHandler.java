c8ee3f0ffb6ff5ba8c11f127b6a4a229859f24ca
chryl <cyl5yy412@gmail.com>
2023-07-24 15:35:42 +0800

    修改测试配置文件

--- src/main/java/com/haiyisoft/handler/NGDHandler.java
+++ src/main/java/com/haiyisoft/handler/NGDHandler.java
@@ -70,7 +70,7 @@ public class NGDHandler {
             log.error("百度知识调用异常 code: {} , msg: {} , answer: {}", code, msg, answer);
         }
         //context全局交互实体
-        JSONObject context = result.getJSONObject("data").getJSONObject("context");//context
+        JSONObject context = result.getJSONObject("data").getJSONObject("context");
         //测试发现闲聊时,无context
         if (context != null) {
             //处理用户校验
@@ -78,7 +78,7 @@ public class NGDHandler {
             //处理客户意图
             NGDUtil.handlerIntent(context, ngdEvent);
         }
-        
+
         //处理记录会话
         NGDUtil.convertNgdNodeMateData(xccRecognitionResult, answer, result, ngdEvent);
 
